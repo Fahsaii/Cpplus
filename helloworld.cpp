8cf902b150ee26db484a933eeebd411fcffff4b6
Fahsaii <97835163+Fahsaii@users.noreply.github.com>
2023-07-10 10:40:43 +0700

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " Student Code : 66-060216-3004-5 "<<endl;
+	cout << " Student Name : Fahsai Thongpakdee "<<endl;
+	cout << "     Division : Information Technology "<<endl;
+	cout << "         Year : 1"<<endl;
+	cout << "         Room : RA "<<endl;
+	cout << "          Sex : female "<<endl;
+	cout << "          Age : 18 "<<endl;
+	system("pause");
+	return(0);
+}
